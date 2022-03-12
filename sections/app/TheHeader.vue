2d9c4e1d05c2d1394663c108cd3146ef099ba168
kaita1257 <u148873i@ecs.osaka-u.ac.jp>
2022-03-12 10:26:01 +0900

    change title and tab size

--- sections/app/TheHeader.vue
+++ sections/app/TheHeader.vue
@@ -1,14 +1,23 @@
 <template>
-  <v-app-bar dark dense height="64" fixed>
-    <v-toolbar-title :class="brandNameFontWeight">{{
-      brandName
-    }}</v-toolbar-title>
+  <v-app-bar dark dense height="72" fixed>
+    <v-toolbar-title :class="brandNameFontWeight + ' text-h6 text-lg-h5'">
+      {{ brandName }}
+    </v-toolbar-title>
+
     <v-spacer></v-spacer>
     <router-link to="/" class="nt-app-bar-link">
-      <v-btn elevation="0">活動内容</v-btn>
-      <v-btn elevation="0">戦績</v-btn>
-      <v-btn elevation="0">部員紹介</v-btn>
+      <v-btn elevation="0" class="text-subtitle-1">活動内容</v-btn>
+      <v-btn elevation="0" class="text-subtitle-1">新歓情報</v-btn>
+      <v-btn elevation="0" class="text-subtitle-1">部員紹介</v-btn>
+      <v-btn elevation="0" class="text-subtitle-1">質問</v-btn>
     </router-link>
+    <a
+      href="http://ouachp.web.fc2.com/report1.html"
+      class="text-decoration-none"
+      target="_blank"
+    >
+      <v-btn elevation="0" class="text-subtitle-1">リザルト</v-btn>
+    </a>
   </v-app-bar>
 </template>
 
