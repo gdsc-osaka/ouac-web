d8517fd618df776f0c8eaf2ab1dc3ffeb784b8be
kaita1257 <u148873i@ecs.osaka-u.ac.jp>
2022-03-14 15:03:03 +0900

    change text of welcome event section

--- sections/top/WelcomeEventSection.vue
+++ sections/top/WelcomeEventSection.vue
@@ -3,25 +3,37 @@
     <v-col cols="12" sm="6">
       <h1 class="font-weight-bold">{{ title }}</h1>
       <ul class="d-block text-left text-h5">
-        <li>日程：毎週金曜日 18:00～</li>
-        <li>場所：自動車部ガレージ</li>
-        <li>イベント名：部会/見学会</li>
-        <li>内容：ガレージ見学→ごはん→ドライブ</li>
+        <li class="ml-15">
+          日程：毎週金曜日 18:00～
+          <ul class="ml-12">
+            <li>昼過ぎからは誰かしら居ることが多いので、</li>
+            <li>早めの時間でもお気軽にお越しください。</li>
+          </ul>
+        </li>
+        <li class="ml-15">場所：自動車部ガレージ</li>
       </ul>
-
-      <!--<div class="mb-2">
+      <div class="mb-2">
         <v-btn
           style="text-transform: none"
           large
           rounded
           class="white--text"
+          href="https://goo.gl/maps/oqWK5rYEbZYUxoCT7"
           :color="btnColor"
-          :to="link"
         >
           {{ btnTxt }}
         </v-btn>
       </div>
-      <small>{{ subTxt }}</small>-->
+      <ul class="d-block text-left text-h5">
+        <li class="ml-n3">イベント名：部会&新歓ドライブ</li>
+        <li class="ml-15">
+          内容：ガレージ見学
+          <ul class="ml-12">
+            <li>→晩ごはん</li>
+            <li>→ドライブ(部員が家まで車で送ります!)</li>
+          </ul>
+        </li>
+      </ul>
     </v-col>
   </v-row>
 </template>
@@ -40,7 +52,7 @@ export default {
     },
     btnTxt: {
       type: String,
-      default: 'Sample Button Text',
+      default: 'Google Map',
     },
     subTxt: {
       type: String,
@@ -60,4 +72,7 @@ export default {
 
 <style lang="scss">
 @import '@/assets/styles/app';
+ul {
+  list-style: none;
+}
 </style>
