94f1e2505ca87dd1389d026be34c8ac9fbf66d6a
kaita1257 <u148873i@ecs.osaka-u.ac.jp>
2022-03-17 21:43:38 +0900

    change header and delete gonuxt section

--- assets/config.js
+++ assets/config.js
@@ -4,17 +4,24 @@ export default {
     nameShort: 'OUAC 大阪大学自動車部新歓ページ',
     footer: {
       links: [
-        { icon: 'mdi-twitter', link: 'https://twitter.com/' },
+        { icon: 'mdi-twitter', link: 'https://twitter.com/ouachp' },
         {
           icon: 'mdi-instagram',
-          link: 'https://www.instagram.com/',
+          link: 'https://www.instagram.com/ouac_220/',
         },
-        { icon: 'mdi-github', link: 'https://github.com/gdsc-osaka' },
+        {
+          icon: 'mdi-facebook',
+          link: 'https://www.facebook.com/ouac.official',
+        },
+        {
+          icon: 'mdi-youtube',
+          link: 'https://www.youtube.com/user/ouachp',
+        },
       ],
     },
     copyrightInfo: {
       year: 2022,
-      owner: 'Sample Club',
+      owner: '大阪大学自動車部',
     },
   },
   // light: {
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,6 @@
 <template>
   <v-container fluid>
     <TopSec />
-    <GonuxtSec />
   </v-container>
 </template>
 
