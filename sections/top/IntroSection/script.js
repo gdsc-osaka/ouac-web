13cb93404d8cd7c192291611574a2d78137404db
kaita1257 <u148873i@ecs.osaka-u.ac.jp>
2022-03-29 22:53:08 +0900

    add result in menu

--- sections/top/IntroSection/script.js
+++
@@ -1,6 +0,0 @@
-export default {
-  name: 'IntroSection',
-  data: () => ({
-    imgSrc: require('@/assets/top/img/intro.jpg'),
-  }),
-}
--- sections/top/IntroSection2/script.js
+++ sections/top/IntroSection2/script.js
@@ -1,6 +1,3 @@
 export default {
   name: 'IntroSection2',
-  data: () => ({
-    imgSrc: require('@/assets/top/img/intro.jpg'),
-  }),
 }
