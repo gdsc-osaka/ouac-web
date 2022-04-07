7a9f9406393845e421aeea88af071d17dced90ab
kaita1257 <u148873i@ecs.osaka-u.ac.jp>
2022-04-07 22:38:56 +0900

    Updates

---
+++ _nuxt/c0ce4ce.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[22],{500:function(n,o,e){"use strict";e.r(o),o.default={name:"IntroSection2"}}}]);
--- _nuxt/ecda0f5.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[22],{499:function(n,o,e){"use strict";e.r(o),o.default={name:"IntroSection2"}}}]);
